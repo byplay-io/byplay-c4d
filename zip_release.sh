4f4523bbdd5f778ca56659242f6e0aa951f31876
Vadim Lobanov <vadim@lobanov.pw>
2021-05-21 17:23:56 +0300

    fix packing

--- zip_release.sh
+++ zip_release.sh
@@ -2,4 +2,6 @@
 
 echo "VERSION = '$1'.split('/')[-1]" > package/python2/byplay/version.py
 echo "VERSION = '$1'.split('/')[-1]" > package/python3/byplay/version.py
-zip -r package.zip package/*.pyp package/python2/**/*.py package/python3/**/*.py
+
+cd package
+zip -r ../package.zip *.pyp python2/**/*.py python3/**/*.py
